bca966090251448bdf3a73a3f902dc3c3e9f5a6c
Scorpio Software <scorpiosoftware666@gmail.com>
2025-06-01 21:14:31 +0300

    Fix take away description translation in Arabic localization

--- lang/ar/entry.php
+++ lang/ar/entry.php
@@ -4,7 +4,7 @@ return [
         'title' => 'كيف تريد طلبك؟',
         'subtitle' => 'يرجى اختيار خيار التناول المفضل لديك',
         'take_away' => 'طلب خارج المطعم',
-        'take_away_description' => '(سفاري)الطلب خارج المطعم',
+        'take_away_description' => '(سفاري)',
         'dine_in' => 'التناول في المطعم',    
         'dine_in_description' => 'استمتع بوجبتك في مطعمنا',
         'cancel' => 'إلغاء',
