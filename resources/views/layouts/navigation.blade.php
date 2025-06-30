9ffacff354274c9035526fa4cfd4f2748aa24b41
Scorpio Software <scorpiosoftware666@gmail.com>
2025-06-30 23:05:28 +0300

    fix: Update navigation link for ads to point to tables and use correct localization string

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -106,7 +106,7 @@
             </x-responsive-nav-link>
         </div>
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('ads.index')" :active="request()->routeIs('ads.index')">
+            <x-responsive-nav-link :href="route('table')" :active="request()->routeIs('table')">
                 {{ __('admin-panel.tables') }}
             </x-responsive-nav-link>
         </div>
